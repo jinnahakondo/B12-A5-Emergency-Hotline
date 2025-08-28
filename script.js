3b2c38732926ef424fc9efa74673fbce09a0a554
jinnahakondo <mdjinnahakondo@gmail.com>
2025-08-28 12:27:42 +0600

    responsive header section added

---
+++ script.js
@@ -0,0 +1,8 @@
+document.getElementById("manubar").addEventListener("click", function () {
+    document.getElementById("manu-items").style.top = "0";
+
+})
+document.getElementById("manubar-hidden").addEventListener("click", function () {
+    document.getElementById("manu-items").style.top = "-500px";
+
+})
